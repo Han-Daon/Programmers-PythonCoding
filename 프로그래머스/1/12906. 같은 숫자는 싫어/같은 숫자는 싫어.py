694ad243369d093de6a2b3a32b83f3a56583f2ee
Dan <138564304+Han-Daon@users.noreply.github.com>
2024-01-18 20:28:37 +0900

    Update 같은 숫자는 싫어.py

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -4,3 +4,7 @@ def solution(arr):
         if [arr[i]] != arr[i + 1: i + 2]:
             answer.append(arr[i])
     return answer
+
+# 추가로 알게 된 부분: 슬라이싱을 하면 [] 배열 값으로 나옴!
+     - arr[:]  = [값]
+     - 비교하는 값을 배열로 감싸줌: [arr[i]]
